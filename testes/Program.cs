1277ab1b46409b49517ff17162bd27ca3c8810ad
rita <maria.rita27012@gmail.com>
2022-04-28 10:02:42 -0300

    Adicionar arquivos de projeto.

---
+++ testes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int cod, horas;
+            float vlrecebido, salario;
+
+            Console.Write("Digite o código do funcionário: ");
+            cod = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o tempo de horas trabalhado: ");
+            horas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor recebido por hora: ");
+            vlrecebido = float.Parse(Console.ReadLine());
+
+            salario = horas * vlrecebido;
+
+            Console.WriteLine("O salário é de: "+salario);
+            Console.ReadKey();
+        }
+    }
+}
