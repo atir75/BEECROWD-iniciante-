792ebf6720bcf39694044b4bb9dc7cc95d97f295
rita <maria.rita27012@gmail.com>
2022-04-28 11:19:18 -0300

    segundo commit

--- testes/Program.cs
+++ testes/Program.cs
@@ -10,23 +10,118 @@ namespace testes
     {
         static void Main(string[] args)
         {
+            ///*1000*/
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+
+
+            //1001
+            //int A, B, X;
+            //Console.Write("Digite um valor: ");
+            //A = int.Parse(Console.ReadLine());
+            //Console.Write("Digite um segundo valor: ");
+            //B = int.Parse(Console.ReadLine());
+
+            //X = A + B;
+            //Console.WriteLine("X = " + X);
+            //Console.ReadLine();
+
+
+
+            //1002
+
+            //float raio, area;
+            //Console.Write("Digite o raio: ");
+            //raio = float.Parse(Console.ReadLine());
+
+            //area = (float)3.14 * (raio * raio);
+            //Console.WriteLine("A area é igual a: " + area);
+            //Console.ReadLine();
+
+
+            //1003
+            //int a, b, soma;
+            //Console.Write("Digite o primeiro valor: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o segundo valor: ");
+            //b = int.Parse(Console.ReadLine());
+
+            //soma = a + b;
+            //Console.WriteLine("SOMA= " + soma);
+            //Console.ReadKey();
+
+
+
+            //1004
+            //int a, b, prod;
+            //Console.Write("Digite o primeiro valor: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o segundo valor: ");
+            //b = int.Parse(Console.ReadLine());
+
+            //prod = a * b;
+            //Console.WriteLine("PROD = " + prod);
+
+
+
+            //1005
+            //float A, B, media;
+            //Console.Write("Digite a primeira nota: ");
+            //A = float.Parse(Console.ReadLine());
+            //Console.Write("Digite a segunda nota: ");
+            //B = float.Parse(Console.ReadLine());
+
+            //media = ((float)3.5 * A) + ((float)7.5 * B)/2;
+
+            //Console.WriteLine("MEDIA = " + media);
+            //Console.ReadLine();
+
+
+
+            //1006
+            //int A, B, C;
+            //Console.Write("Digite a primeira nota: ");
+            //A = int.Parse(Console.ReadLine());
+            //Console.Write("Digite a segunda nota: ");
+            //B = int.Parse(Console.ReadLine());
+            //Console.Write("Digite a terceira nota: ");
+            //C = int.Parse(Console.ReadLine());
+
+            //int media = (A * 2) + (B * 3) + (C * 5) / 3;
+            //Console.WriteLine("MEDIA = " + media);
+            //Console.ReadKey();
+
+
+
+            //1007
+
+
+
+
+            ///*1008*/
+            //int cod, horas;
+            //float vlrecebido, salario;
+
+            //Console.Write("Digite o código do funcionário: ");
+            //cod = int.Parse(Console.ReadLine());
+
+            //Console.Write("Digite o tempo de horas trabalhado: ");
+            //horas = int.Parse(Console.ReadLine());
+
+            //Console.Write("Digite o valor recebido por hora: ");
+            //vlrecebido = float.Parse(Console.ReadLine());
+
+            //salario = horas * vlrecebido;
+
+            //Console.WriteLine("O salário é de: "+salario);
+            //Console.ReadKey();
+
             
-            int cod, horas;
-            float vlrecebido, salario;
 
-            Console.Write("Digite o código do funcionário: ");
-            cod = int.Parse(Console.ReadLine());
+            
+            /*1009*/
 
-            Console.Write("Digite o tempo de horas trabalhado: ");
-            horas = int.Parse(Console.ReadLine());
-
-            Console.Write("Digite o valor recebido por hora: ");
-            vlrecebido = float.Parse(Console.ReadLine());
-
-            salario = horas * vlrecebido;
-
-            Console.WriteLine("O salário é de: "+salario);
-            Console.ReadKey();
         }
     }
 }
